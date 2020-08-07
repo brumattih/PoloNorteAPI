6f6bb1b6d5cde3cc05b79c675667997ae4bd78a0
brumattih <hrpreis@gmail.com>
2020-08-06 21:58:18 -0300

    fix on services

--- src/services/equipments.js
+++ src/services/equipments.js
@@ -3,7 +3,6 @@ const repository = require('../repositories/equipments')
 
 const getById = async (id) => {
     const equipment = await repository.getById(id)
-    console.log(equipment)
     if (!equipment) {
         throw { status: 404, message: "Not found" }
     }
